c10ec8c6e07cf051a8842a4fba8d9543cbb2f841
FELIPE <and.veg.fel@gmail.com>
2024-12-13 16:03:23 -0500

    Laboratorio 3

---
+++ mis.js
@@ -0,0 +1,33 @@
+function segura() {
+    pwd = prompt( "Contraseña" ) ;
+
+    vcv = 0 ;       // CONTADORA DE VOCALES
+    vcc = 0 ;       // CONTADORA DE CONSONANTES
+
+    for( i=0 ; i<pwd.length ; i=i+1 ) {
+        if( pwd[i] == "a" || pwd[i] == "e" || pwd[i] == "i" || pwd[i] == "o" || pwd[i] == "u" ) {
+            vcv = vcv + 1 ;
+        } else {
+            vcc = vcc + 1 ;
+            vcv = 0 ;
+        }
+
+        if( vcc >= 3 ) {
+            break ;
+        }
+        if( vcv >= 3 ) {
+            break ;
+        }
+    }
+
+    if( vcv >= 3 ) {
+        document.write( "CONTRASEÑA NO SEGURA Por que tiene 3 vocales" ) ;
+    } else {
+        if( vcc >= 3 ) {
+            document.write( "CONTRASEÑA NO SEGURA pq 3CC" ) ;
+        } else {
+            document.write( "CONTRASEÑA SEGURA" ) ;
+        }
+    }
+
+}
